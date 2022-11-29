def8bc28e07cba87796b3e7abde12a62a67d1d73
Grant Crawford <grantttt.crawford@gmail.com>
2022-11-28 18:26:21 -0600

    initial missions

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response){
+        response.json().then(function(json){
+            const astronaut = json;
+            console.log(astronaut)
+            
+            const list = document.getElementById("container")
+            for (let i = 0; i<astronaut.length; i++){
+                list.innerHTML += `<div class="astronaut">
+                <div class="bio">
+                <h3>${astronaut[i].firstName} ${astronaut[i].lastName}</h3>
+                <ul>
+                <li>Hours in space: ${astronaut[i].hoursInSpace}</li>
+                <li id="active">Active: ${astronaut[i].active}</li>
+                <li>Skills: ${astronaut[i].skills}</li>
+                </ul>
+                </div>
+                <img class="avatar" src="${astronaut[i].picture}">
+                </div>` 
+        }
+       
+            
+            
+        })
+    })
+})
