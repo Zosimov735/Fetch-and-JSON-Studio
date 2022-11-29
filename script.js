4f9a2b7b59c7d1e70c9d9a59ec902f7328f3ee8f
Grant Crawford <grantttt.crawford@gmail.com>
2022-11-28 19:21:05 -0600

    bonus missions complete

--- script.js
+++ script.js
@@ -4,24 +4,36 @@ window.addEventListener("load", function(){
         response.json().then(function(json){
             const astronaut = json;
             console.log(astronaut)
+            astronaut.sort((a,b)=> (a.hoursInSpace < b.hoursInSpace ? 1 : -1))
+            function activeCheck(active){
+                if (active){
+                return `<span id="active" style="color: green">${astronaut[i].active}</span>`
+            }    
+            else{
+                return `${astronaut[i].active}`
+            }
+        }
             
             const list = document.getElementById("container")
-            for (let i = 0; i<astronaut.length; i++){
+            for (var i = 0; i<astronaut.length; i++){
                 list.innerHTML += `<div class="astronaut">
                 <div class="bio">
                 <h3>${astronaut[i].firstName} ${astronaut[i].lastName}</h3>
                 <ul>
                 <li>Hours in space: ${astronaut[i].hoursInSpace}</li>
-                <li id="active">Active: ${astronaut[i].active}</li>
+                <li>${activeCheck(astronaut[i].active)}</li>
                 <li>Skills: ${astronaut[i].skills}</li>
                 </ul>
                 </div>
                 <img class="avatar" src="${astronaut[i].picture}">
-                </div>` 
-        }
-       
+                </div>`  
+                activeCheck(i)  
+            }
+            list.innerHTML += `<div><h1>Astronaut Count: ${astronaut.length}</h1></div>`
+            
             
             
         })
     })
+    
 })
